539674bb9a004c178da2cc3e968712027d86ae6e
Julia Irvine <jlavenderpanic@gmail.com>
2024-06-07 13:29:37 +0100

    Add config system and profiles

--- bot.py
+++ bot.py
@@ -2,20 +2,11 @@ import discord
 from discord.ext import tasks
 
 from datetime import datetime, timedelta
-import os
+import os, json
 
-# The message to watch for reactions
-WATCHED_MESSAGE_ID = 1243247452765552752
-
-# The first role to be given, upon reacting to the message
-GIVEN_ROLE_TIER1_ID = 1243240134909100082
-
-# The second role to be given, upon having the first role and being in the server for WAITTIME mins
-GIVEN_ROLE_TIER2_ID = 1226488470181253220
-
-# Channel to print messages to, and guild to join for role data
-DEBUG_CHANNEL = 1243239439266877570
-GUILD_ID = 496183179510677505
+DEBUG = True
+VERSION = -1
+config = None
 
 WAITTIME = 1440 # 1 day = 1440 mins
 
@@ -26,35 +17,34 @@ intents.members = True
 
 class ErasureClient(discord.Client):
     async def debug_message(self, message):
-        channel = self.get_channel(DEBUG_CHANNEL)
+        channel = self.get_channel(config['debug_channel'])
         await channel.send(message)
     
     # We can't be guaranteed that the message we want to listen on will be in the cache, so we need to use the raw reaction add here.
     async def on_raw_reaction_add(self, event): 
-        if event.message_id != WATCHED_MESSAGE_ID: 
+        if event.message_id != config['watched_message']: 
             return # The reaction is on a different message
         member = event.member
 
-        if member.get_role(GIVEN_ROLE_TIER1_ID) == None and member.get_role(GIVEN_ROLE_TIER2_ID) == None: # If they don't already have the role, or the t2 version...
-            role = self.guild.get_role(GIVEN_ROLE_TIER1_ID)
+        if member.get_role(config['given_role_t1']) == None and member.get_role(config['given_role_t2']) == None: # If they don't already have the role, or the t2 version...
+            role = self.guild.get_role(config['given_role_t1'])
             # await self.debug_message("added role to " + member.display_name)
             await member.add_roles(role) # ...give it to them
 
     async def on_ready(self):
-        self.guild = self.get_guild(GUILD_ID)
-        channel = self.get_channel(DEBUG_CHANNEL)
-        await channel.send("Started ErasureBot, listening for reactions on message " + str(WATCHED_MESSAGE_ID) + " giving role " + str(GIVEN_ROLE_TIER1_ID))
+        self.guild = self.get_guild(config['guild_id'])
+        channel = self.get_channel(config['debug_channel'])
+        await channel.send("Booting ErasureOS...")
         
     @discord.ext.tasks.loop(minutes=1)
     async def check_tier2(self):
-        # await self.debug_message("running job!!")
         # Get all the users with the tier1 role...
-        t1_members = self.guild.get_role(GIVEN_ROLE_TIER1_ID).members
+        t1_members = self.guild.get_role(config['given_role_t1']).members
         for member in t1_members:
             # await self.debug_message("for user " + member.display_name + "   " + str(member.joined_at))
-            if datetime.now(member.joined_at.tzinfo) - timedelta(minutes=WAITTIME) > member.joined_at:
-                await member.add_roles(self.guild.get_role(GIVEN_ROLE_TIER2_ID))
-                await member.remove_roles(self.guild.get_role(GIVEN_ROLE_TIER1_ID))
+            if datetime.now(member.joined_at.tzinfo) - timedelta(minutes=config['automatic_role_t2_waittime']) > member.joined_at:
+                await member.add_roles(self.guild.get_role(config['given_role_t2']))
+                await member.remove_roles(self.guild.get_role(config['given_role_t1']))
                 # await self.debug_message("updated role for " + member.display_name)
 
     @check_tier2.before_loop
@@ -63,14 +53,28 @@ class ErasureClient(discord.Client):
         
 
     async def setup_hook(self) -> None:
-        self.check_tier2.start()
+        if config['automatic_role_t2']:
+            self.check_tier2.start()
 
 
 client = ErasureClient(intents=intents)
 
 def main():
+    global config
+
     with open('secret.token', 'r') as file:
         token = file.read().rstrip()
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+    
+    VERSION = config['version']
+    
+    if DEBUG:
+        config = config["debug"]
+    else:
+        config = config["live"]
+
+    print(config)
     client.run(token)
 
 
